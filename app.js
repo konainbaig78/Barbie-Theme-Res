18fc33e523f62f8475ef6247f139373922ecb82a
konainbaig78 <konainbaig915@gmail.com>
2024-09-08 00:54:11 +0500

    working in progress

---
+++ app.js
@@ -0,0 +1,8 @@
+function remove() {
+  const main = document.getElementById("main");
+  main.style.borderBottom = "none";
+}
+function add() {
+  const main = document.getElementById("main");
+  main.style.borderBottom = "2px solid #EE098E";
+}
